ce3784404a220d3bf608b898562b767ff29dfdcb
Sarah Rogers <s.rogers0715@yahoo.com>
2020-07-12 15:48:43 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+ def badge_maker(name)
+   "Hello, my name is #{name}."
+ end
+ 
+ def batch_badge_creator(attendees)
+  badges= []
+  attendees.each do|name|
+   badges<< "Hello, my name is #{name}."
+  end
+ return badges
+end
+
+def assign_rooms(room_assignments)
+  badges=[]
+  room_assignments.each do|name|
+  room_assignments<< "Hello #{name}!"
+ end
+ room_assignments
+end
+
+
+
