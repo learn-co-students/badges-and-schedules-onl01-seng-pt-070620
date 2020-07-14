4042ea626e06f1394e8587f41910e756de808452
Jill Short <jillian.b.short@gmail.com>
2020-07-14 06:37:26 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+def badge_maker (name=attendees)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (badges)
+  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  attendees.each do |attendee|
+    print "Hello, my name is #{attendee}."
+  end
+end
