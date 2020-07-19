041c4907c423337247e262b6cf11145239ec5679
Dayami Velazquez <dayami.velazquez@gmail.com>
2020-07-19 09:07:00 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,9 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  attendees.each_with_index.map do 
-  puts assignment
-end 
+  attendees.each_with_index.map {|name,index|"Hello, #{name}! You'll be assigned to room #{index+1}!"}
 end
+
+  
+ 
+
