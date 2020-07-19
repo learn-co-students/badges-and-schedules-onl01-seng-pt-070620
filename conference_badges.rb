41a1024a59fa64f8ae554b8f33784b046fcebeb4
Dayami Velazquez <dayami.velazquez@gmail.com>
2020-07-19 08:59:55 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,3 +3,15 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+  attendees=["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  
+def batch_badge_creator(attendees)
+  attendees.collect {|name| badge_maker(name)}
+end
+
+def assign_rooms(attendees)
+  attendees.each_with_index.map do 
+  puts assignment
+end 
+end
